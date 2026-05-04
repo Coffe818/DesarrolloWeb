0405eeddadb98ae184c83de8554704828a9a430f
Jose Grimaldo <josemanuelgrimaldovalero@gmail.com>
2026-05-04 15:09:30 -0600

    quitar comentarios

--- FINAL/vark-app/src/app/sidebar/sidebar.ts
+++ FINAL/vark-app/src/app/sidebar/sidebar.ts
@@ -7,15 +7,12 @@ import { Component, signal, inject, DOCUMENT } from '@angular/core';
   styleUrl: './sidebar.css',
 })
 export class SidebarComponent {
-  // Señal para el modo oscuro (camelCase minúscula)
   isDarkMode = signal<boolean>(false);
 
   isSidebarOpen = false;
-  // Inyectamos el DOCUMENT para cambiar el <html>
   private document = inject(DOCUMENT);
 
   constructor() {
-    // Leemos el tema guardado en localStorage
     const savedTheme = localStorage.getItem('theme');
     if (savedTheme === 'dark') {
       this.isDarkMode.set(true);
@@ -27,7 +24,6 @@ export class SidebarComponent {
     this.isSidebarOpen = !this.isSidebarOpen;
   }
 
-  // Método para cambiar el tema (camelCase con mayúscula inicial)
   ToggleTheme(): void {
     const newTheme = !this.isDarkMode();
     this.isDarkMode.set(newTheme);
@@ -35,7 +31,6 @@ export class SidebarComponent {
     this.AplicarTema();
   }
 
-  // Función privada para aplicar el atributo al <html>
   private AplicarTema(): void {
     if (this.isDarkMode()) {
       this.document.documentElement.setAttribute('data-bs-theme', 'dark');
